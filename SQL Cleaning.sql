86f0a531e10aa2735adfc74d8da8238b8fec4c0b
Jorge lm <121949979+JLMie@users.noreply.github.com>
2023-02-24 00:50:36 +0100

    Update SQL Cleaning.sql

--- SQL Cleaning.sql
+++ SQL Cleaning.sql
@@ -1,5 +1,9 @@
+		   	  /* The entirety of the content within the files used throughout the entire portfolio is fictitious.*/
+		          /* All of the Excel CSV files used to create this section are available in the Github repository. */
+					          
+       /* The aim of this project is to clean and standrized the data and make the dataset more usable and easier to observe it. */
 
--- Creating a Table with errors on the values and strings. Let's fix it!
+                                         // CREATING A TABLE WITH MISTAKES AND FIXING 4 ERROR TYPES //
 
 CREATE TABLE Errorstablefix (
 EmployeeID varchar(50)
@@ -13,44 +17,72 @@ Insert into Errorstablefix Values
 ,('802', '16','Production', 'Operator')
 ,('805', '19','MarkEting', 'Manager-Updated')
 
+-- Here you can display the table.
 SELECT*
 FROM Errorstablefix
--- We can see 3 error type.  
 
--- Spaces is a wrong place. 
+-- 1.Error: spaces is a wrong place. 
 SELECT EmployeeID, TRIM(EmployeeID) AS IDfixed
 FROM Errorstablefix
 
--- An xtra string we dont need. ("-Updated")
+-- 2.Error: an xtra string we dont need. ("-Updated")
 Select Position, REPLACE(Position, '-',' ') AS PositionFixed
 FROM Errorstablefix
 
--- A misspelled word. (Naintenance)
+-- 3. Error: A misspelled word. (Naintenance)
 Select Department, REPLACE(Department, 'Naintenance','Maintenance') AS PositionFixed
 FROM Errorstablefix
 
--- Double Uppercase. (MArketing)
+-- 4.Error: Double Uppercase. (MArketing)
 SELECT Department, UPPER(LEFT(Department, 1)) + SUBSTRING(Department, 2, len(Department)) as FixDepartmentName
 FROM Errorstablefix
 
--- Let's come back to the database used for cleaninng section.
--- Identifing where NULL values are to start have a look at empty gaps. 
--- Assumption: We already know that some data in Education field from OldEmployee are missing and we need to ask for those data to
--- complete a new dataset.
 
+                                                      // DELETING UNUSED COULMNS //
+
+SELECT*
+FROM Trial.dbo.CleaningdataPort
+
+ALTER TABLE Trial.dbo.CleaningdataPort
+DROP COLUMN Kids, Emp_Address2
+
+
+                                                   // POPULATING "EDUCATION FIELD" //
+
+		/* We need a table without the same OldEmployeeID in two rows. However, looking at the values in their columns, 
+values in EducationField are missing in one of those duplicate rows. So, EducationField needs to be added to the resultant row. */
+
+						
+-- Here, we discover EducationField is the one is missing in one row. 
 SELECT*
 FROM Trial.dbo.CleaningdataPort
 WHERE EducationField is null
 ORDER BY OldEmployee_ID
 
+-- We need to join the table itself for removing OldEmployee_ID duplicates and merge the EducationField data are missing.
+SELECT a.Employee_ID, a.EducationField, b.Employee_ID, b.EducationField
+FROM Trial.dbo.CleaningdataPort a
+JOIN Trial.dbo.CleaningdataPort b
+     on a.OldEmployee_ID = b.OldEmployee_ID
+	 AND a.[Employee_ID] <> b.[Employee_ID]
+WHERE a.EducationField is null;
+
+-- Let'apply the changes to the table.
+UPDATE a
+SET EducationField = ISNULL(a.EducationField, b.EducationField)
+FROM Trial.dbo.CleaningdataPort a
+JOIN Trial.dbo.CleaningdataPort b
+     on a.OldEmployee_ID = b.OldEmployee_ID
+	 AND a.[Employee_ID] <> b.[Employee_ID]
+WHERE a.EducationField is null
+
+
+                                                  // SPLITING "ADDRESS" IN DIFFERENT COLUMNS //
+
 SELECT*
 FROM Trial.dbo.CleaningdataPort
--- On the table, we see a long address in a one single field. We need to use the Eircode and the town name but, 
--- We dont want to remove the data left. So, break out Address into three separate in columns.
--- 1. EirCode/Postcode
--- 2. Region
--- 3. Country
 
+--Original format: T23,CORK CITY, CORK COUNTY, Country. 
 SELECT 
 PARSENAME(REPLACE(Emp_Address, ',','.'),4) AS 'Eir/post Code'
 ,PARSENAME(REPLACE(Emp_Address, ',','.'),3) AS 'Town'
@@ -59,49 +91,27 @@ PARSENAME(REPLACE(Emp_Address, ',','.'),4) AS 'Eir/post Code'
 FROM Trial.dbo.CleaningdataPort;
 
 
+                                                  // OBTEIN MEANING FROM NUMBERS OUTCOME //
 
-SELECT
-FROM Trial.dbo.CleaningdataPort
-
---Now try to keep what is displayed as a change in the table.
-
-
-
-
-
-
-
-
---2 VERSION. Replacing commas for points just in 2/3 to see the differents, like this PARSAME function should work without REPLACE.......
-
-
---3 version
---Change Y and N to Yes and No in "Sold as Vacant" field. 
-
-
-SELECT DISTINCT (Work_remotely), Count (Work_remotely)
-FROM Trial.dbo.CleaningdataPort
-GROUP BY Work_remotely
-
-
-SELECT Work_remotely
-, CASE WHEN Work_remotely = 'Yes' THEN 'Y'
-       WHEN Work_remotely = 'No'  THEN 'N'
-	   END
-FROM Trial.dbo.CleaningdataPort
-
+-- How many employees work remotely?
 SELECT Work_remotely, COUNT (Work_remotely)
 FROM Trial.dbo.[01Portfolio]
 GROUP BY Work_remotely;
 
---#PENDING Make more sense having different answer (Y, N, O and 1) and unify them as Yes and No.
---Remove duplicates. DOES NOT WORK WITH THE "WITH LINE" (773)
+-- Let's clarify it replacing 0 and 1 for YES and NO respectively. 
+SELECT Work_remotely, Employee_ID,
+CASE WHEN Work_remotely = 0 THEN 'Yes'
+	 ELSE 'No' END AS Work_remotely
+FROM Trial.dbo.CleaningdataPort;
+
+
+                                                        // REMOVING ALL DUPLICATES //
 
 WITH RowNumCTE AS(
 SELECT *,
-     ROW_NUMBER() OVER(
+     ROW_NUMBER() OVER (
 	 PARTITION BY OldEmployee_ID, 
-	              Kids, 
+				  Position,
 	              HireDate, 
 				  Satisfaction_level 
 				  ORDER BY
@@ -109,34 +119,23 @@ SELECT *,
 				       ) row_num
 
 FROM Trial.dbo.CleaningdataPort
-
-
-WITH RowNumCTEE AS(
-SELECT *,
-     ROW_NUMBER() OVER(
-	 PARTITION BY ParcelID, 
-	              PropertyAddress, 
-	              SalePrice,
-				  SaleDate,
-				  LegalReference 
-				  ORDER BY
-				    UniqueID
-				       ) row_num
-FROM Trial.dbo.Nashville
---ORDER BY ParcelID
 )
-SELECT*
+SELECT* --In case a duplicate rows show up and we want delete them. 
+--We would need to replace the previous 'SELECT' for 'DELETE' and remove as well the "*" before run it. 
 FROM RowNumCTE
-WHERE row_num > 1
-ORDER BY PropertyAddress
-
---Delete Unused Columns.
+WHERE row_num > 2
+ORDER BY Employee_ID
 
 SELECT*
 FROM Trial.dbo.CleaningdataPort
 
-ALTER TABLE Trial.dbo.CleaningdataPort
-DROP COLUMN Kids, Emp_Address2
+
+                                                        // EXPORTING THE TABLE //
 
 EXEC master.sys.sp_MSset_oledb_prop;
 
+                                                         THANKS FOR VISITING!
+
+                                          .   . ........ Continuing Learning ........ .   .
+					  
+					  
